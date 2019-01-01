53475c9c9df09e4d6f85538f931b6c072c9adc86
Josh Payne <snare117@gmail.com>
2019-01-01 02:55:29 -0600

    add impact, exploitability graphs, max-flow for calculating respective scores

--- circuit.py
+++ circuit.py
@@ -1,3 +1,7 @@
+'''
+$ python circuit.py -d "Router,<device_1>,<device_2>,...,<device_n>"
+'''
+
 import request
 import json
 import random
@@ -10,76 +14,108 @@ import itertools
 import matplotlib.pyplot as plt
 from optparse import OptionParser
 
-### Things the attacker might be aiming to get. ###
+### Things the attacker might be aiming to obtain. ###
+
 attacker_stash = ["Privacy Breach","this:Root Privileges","this:GPG Key","Credentials","this:Availability","this:Config File","Google Account Access","Physical Location","Privacy Breach"]
+
+def colorVertex(value):
+    if value<2:
+        return 'greenyellow'
+    if value<4:
+        return 'yellow3'
+    if value<6:
+        return 'orange2'
+    if value<8:
+        return 'red3'
+    return 'purple4'
+
 
 '''
 A vector is a list of information about the vulnerabilities of a device, based on its corresponding
 CVE score.
 '''
 
-def buildVector(device_search):
-    if device_search == "Router": return ""
-    vector = {
-        "base": [],
-        "attack_vectors": []
-    }
+def buildVector(device_search,vector):
+    if device_search == "Router": return vector
     with open("known_cves.json") as cve_json:
         cve_data = json.load(cve_json)
         for cve in cve_data[device_search]:
-            # print(cve["cve"])
-
+            vector[cve["cve"]["CVE_data_meta"]["ID"]] = {}
             try:
-                vector["base"].append(cve["impact"]["baseMetricV3"]["cvssV3"]["baseScore"])
-                vector["attack_vectors"].append(cve["impact"]["baseMetricV3"]["cvssV3"]["vectorString"])
+                vector[cve["cve"]["CVE_data_meta"]["ID"]]["base"] = cve["impact"]["baseMetricV3"]["cvssV3"]["baseScore"]
+                vector[cve["cve"]["CVE_data_meta"]["ID"]]["impact"] = cve["impact"]["baseMetricV3"]["impactScore"]
+                vector[cve["cve"]["CVE_data_meta"]["ID"]]["exploitability"] = cve["impact"]["baseMetricV3"]["exploitabilityScore"]
             except:
                 try:
-                    vector["base"].append(cve["impact"]["baseMetricV2"]["cvssV2"]["baseScore"])
-                    vector["attack_vectors"].append(cve["impact"]["baseMetricV2"]["cvssV2"]["vectorString"])
+                    vector[cve["cve"]["CVE_data_meta"]["ID"]]["base"] = cve["impact"]["baseMetricV2"]["cvssV2"]["baseScore"]
+                    vector[cve["cve"]["CVE_data_meta"]["ID"]]["impact"] = cve["impact"]["baseMetricV2"]["impactScore"]
+                    vector[cve["cve"]["CVE_data_meta"]["ID"]]["exploitability"] = cve["impact"]["baseMetricV2"]["exploitabilityScore"]
+
                 except:
                     continue
-
     return vector
 
 '''
-A circuit is a fully connected, directed graph with subgraphs being circuit elements/IoT devices, vertices being CVEs, and 
-an edge (v_1,v_2) is 'red' if there exists a match between the output of one vertex v_1 
+A circuit is a directed multigraph with subgraphs being circuit elements/IoT devices, vertices being CVEs, and 
+an edge (v_1,v_2) exists if there exists a match between the output of one vertex v_1 
 (CVE) and the input of another vertex v_2 in the graph. The path consisting
 of red edges from one vertex v_m and another vertex v_n is called an attack path.
 
+Vertices are colored based on metrics from the risk score, as are edges that form paths. For path coloring,
+we solve an optimization problem to color paths with highest impact, lowest exploitability, etc.
+
 Here, we use a brute force approach to generate all possible paths, but as vulnerability information
 and system complexity increases, an AI planning or other intelligent approach may be needed.
+
+For determining optimal paths, we solve a max flow/min cut problem.
 ''' 
 
-def buildCircuit(devices):
+def buildCircuit(devices,vector):
 
     ### set up circuit ###
 
-    dotstr = '/*****\nAttack Circuit\n*****/\n\ndigraph G {\n  graph [splines=true overlap=false]\n  node  [shape=ellipse, width=0.3, height=0.3]\n' #  size="30,30";\n ratio="fill"
+    schematic_dotstr = '/*****\nAttack Circuit: Schematic\n*****/\n\ndigraph G {\n  graph [splines=true overlap=false]\n  node  [shape=ellipse, width=0.3, height=0.3]\n' #  size="30,30";\n ratio="fill"
+    impact_dotstr = '/*****\nAttack Circuit: Impact\n*****/\n\ndigraph G {\n  graph [splines=true overlap=false]\n  node  [shape=ellipse, width=0.3, height=0.3]\n' #  size="30,30";\n ratio="fill"
+    exploitability_dotstr = '/*****\nAttack Circuit: Exploitability\n*****/\n\ndigraph G {\n  graph [splines=true overlap=false]\n  node  [shape=ellipse, width=0.3, height=0.3]\n' #  size="30,30";\n ratio="fill"
     attack_circuit = {}
-    G = nx.MultiDiGraph()
+    ImpactGraph = nx.DiGraph()
+    ExploitabilityGraph = nx.DiGraph()
+    SchematicGraph = nx.DiGraph()
+    
     edge_labels = {}
     dev_ind = cve_ind = 0
 
     ### add attacker vertex ###
     
-    G.add_node("Attacker")
-    dotstr += "  " + str(dev_ind) + ' [label="Attacker", shape=Mdiamond];\n'
-    
+    SchematicGraph.add_node("Attacker")
+    ImpactGraph.add_node("Attacker")
+    ExploitabilityGraph.add_node("Attacker")
+
+    schematic_dotstr += "  " + str(dev_ind) + ' [label="Attacker", shape=Mdiamond];\n'
+    impact_dotstr += "  " + str(dev_ind) + ' [label="Attacker", shape=Mdiamond];\n'
+    exploitability_dotstr += "  " + str(dev_ind) + ' [label="Attacker", shape=Mdiamond];\n'
+
     with open("descriptions_io.json") as desc:
         io_desc = json.load(desc)
-        dotstr += '  0 -> 1 [label="Some router/network vulnerability"];\n'
+        schematic_dotstr += '  0 -> 1 [label="Some router/network vulnerability"];\n'
+        impact_dotstr += '  0 -> 1 [label="Some router/network vulnerability"];\n'
+        exploitability_dotstr += '  0 -> 1 [label="Some router/network vulnerability"];\n'
         dotmap = {}
 
-         ### add all vertices (devices) ###
+        ### add all subgraphs, vertices (devices, CVEs) ###
 
         for device in devices:
             dev_ind+=1
-            dotstr += "  subgraph cluster_" + str(dev_ind) + ' {\n  label="'+device+'";'
+            schematic_dotstr += "  subgraph cluster_" + str(dev_ind) + ' {\n  label="'+device+'";'
+            impact_dotstr += "  subgraph cluster_" + str(dev_ind) + ' {\n  label="'+device+'";'
+            exploitability_dotstr += "  subgraph cluster_" + str(dev_ind) + ' {\n  label="'+device+'";'
+
             for cve in io_desc[device]:
+                SchematicGraph.add_node(cve["id"])
+                ImpactGraph.add_node(cve["id"])
+                ExploitabilityGraph.add_node(cve["id"])
                 cve_ind+=1
                 dotmap[cve["id"]] = cve_ind
-                G.add_node(cve["id"])
                 attack_circuit[cve["id"]] = {}
                 attack_circuit[cve["id"]]["inputs"] = []
                 attack_circuit[cve["id"]]["outputs"] = []
@@ -89,8 +125,12 @@ def buildCircuit(devices):
                         attack_circuit[cve["id"]]["outputs"].append(io.split('->')[1])
                     else:
                         attack_circuit[cve["id"]]["outputs"].append(io.split('->')[1])
-                dotstr += "    " + str(cve_ind) + ' [label="' + cve["id"] + '"];\n'
-            dotstr += "  }\n"
+                schematic_dotstr += "    " + str(cve_ind) + ' [label="' + cve["id"] + '"];\n'
+                impact_dotstr += "    " + str(cve_ind) + ' [label="' + cve["id"] + '"];\n'
+                exploitability_dotstr += "    " + str(cve_ind) + ' [label="' + cve["id"] + '"];\n'
+            schematic_dotstr += "  }\n"
+            impact_dotstr += "  }\n"
+            exploitability_dotstr += "  }\n"
 
         ### add all device edges ###       
         
@@ -101,53 +141,88 @@ def buildCircuit(devices):
                         for cve_x_output in attack_circuit[cve_dev_x["id"]]["outputs"]:
                             for cve_y_input in attack_circuit[cve_dev_y["id"]]["inputs"]:
                                 if cve_x_output==cve_y_input:
-                                    G.add_edge(cve_dev_x["id"],cve_dev_y["id"])
+                                    SchematicGraph.add_edge(cve_dev_x["id"],cve_dev_y["id"])
+                                    try:
+                                        ImpactGraph.add_edge(cve_dev_x["id"],cve_dev_y["id"],capacity=vector[cve_dev_y["id"]]["impact"])
+                                        ExploitabilityGraph.add_edge(cve_dev_x["id"],cve_dev_y["id"],capacity=vector[cve_dev_y["id"]]["exploitability"])
+                                    except: # sometimes CVE doesn't have a CVSS score
+                                        ImpactGraph.add_edge(cve_dev_x["id"],cve_dev_y["id"],capacity=3.0)
+                                        ExploitabilityGraph.add_edge(cve_dev_x["id"],cve_dev_y["id"],capacity=3.0)
+                                            
                                     edge_labels[(cve_dev_x["id"],cve_dev_y["id"])] = cve_x_output
-                                    dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> " + str(dotmap[cve_dev_y["id"]]) + ' [label="' + cve_x_output + '"];\n'
-        
+                                    try:
+                                        schematic_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> " + str(dotmap[cve_dev_y["id"]]) + ' [label="' + cve_x_output + '" color="' + colorVertex(vector[cve_dev_x["id"]]["base"]) + '"];\n'
+                                        impact_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> " + str(dotmap[cve_dev_y["id"]]) + ' [label="' + cve_x_output + '" color="' + colorVertex(vector[cve_dev_x["id"]]["impact"]) + '"];\n'
+                                        exploitability_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> " + str(dotmap[cve_dev_y["id"]]) + ' [label="' + cve_x_output + '" color="' + colorVertex(vector[cve_dev_x["id"]]["exploitability"]) + '"];\n'
+                                    except:
+                                        schematic_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> " + str(dotmap[cve_dev_y["id"]]) + ' [label="' + cve_x_output + '" color="' + colorVertex(3.0) + '"];\n'
+                                        impact_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> " + str(dotmap[cve_dev_y["id"]]) + ' [label="' + cve_x_output + '" color="' + colorVertex(3.0) + '"];\n'
+                                        exploitability_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> " + str(dotmap[cve_dev_y["id"]]) + ' [label="' + cve_x_output + '" color="' + colorVertex(3.0) + '"];\n'
+
         ### add all attacker edges ###
 
         for dev_x in devices:
             for cve_dev_x in io_desc[dev_x]:
+                if cve_dev_x["id"]!="Non-CVE info: Router":
+                    ImpactGraph.add_edge(cve_dev_x["id"],"Attacker",capacity=100000.0)
+                    ExploitabilityGraph.add_edge(cve_dev_x["id"],"Attacker",capacity=100000.0)
+                try:
+                    schematic_dotstr += "    " + str(dotmap[cve_dev_x["id"]]) + ' [color="' + colorVertex(vector[cve_dev_x["id"]]["base"]) + '"];\n'
+                    impact_dotstr += "    " + str(dotmap[cve_dev_x["id"]]) + ' [color="' + colorVertex(vector[cve_dev_x["id"]]["impact"]) + '"];\n'
+                    exploitability_dotstr += "    " + str(dotmap[cve_dev_x["id"]]) + ' [color="' + colorVertex(vector[cve_dev_x["id"]]["exploitability"]) + '"];\n'
+                except: # sometimes CVE doesn't have a CVSS score
+                    schematic_dotstr += "    " + str(dotmap[cve_dev_x["id"]]) + ' [color="' + colorVertex(3.0) + '"];\n'
+                    impact_dotstr += "    " + str(dotmap[cve_dev_x["id"]]) + ' [color="' + colorVertex(3.0) + '"];\n'
+                    exploitability_dotstr += "    " + str(dotmap[cve_dev_x["id"]]) + ' [color="' + colorVertex(3.0) + '"];\n'
                 for cve_x_output in attack_circuit[cve_dev_x["id"]]["outputs"]:
                     if cve_x_output.split(r"\n")[0] in attacker_stash:
-                        dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> 0" + ' [label="' + cve_x_output + '"];\n'
+                        try:
+                            schematic_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> 0" + ' [label="' + cve_x_output + '" color="' + colorVertex(vector[cve_dev_x["id"]]["base"]) + '"];\n'
+                            impact_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> 0" + ' [label="' + cve_x_output + '" color="' + colorVertex(vector[cve_dev_x["id"]]["impact"]) + '"];\n'
+                            exploitability_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> 0" + ' [label="' + cve_x_output + '" color="' + colorVertex(vector[cve_dev_x["id"]]["exploitability"]) + '"];\n'
+                        except: # sometimes CVE doesn't have a CVSS score
+                            schematic_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> 0" + ' [label="' + cve_x_output + '" color="' + colorVertex(3.0) + '"];\n'
+                            impact_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> 0" + ' [label="' + cve_x_output + '" color="' + colorVertex(3.0) + '"];\n'
+                            exploitability_dotstr += "  " + str(dotmap[cve_dev_x["id"]]) + " -> 0" + ' [label="' + cve_x_output + '" color="' + colorVertex(3.0) + '"];\n'
 
         ### build attack paths ###
 
-        dotstr += r'  label = "\nAttack Circuit\n";  fontsize=24;' + '\n}'
+        schematic_dotstr += r'  label = "\nAttack Circuit: Schematic\n";  fontsize=24;' + '\n}'
+        impact_dotstr += r'  label = "\nAttack Circuit: Impact\n";  fontsize=24;' + '\n}'
+        exploitability_dotstr += r'  label = "\nAttack Circuit: Exploitability\n";  fontsize=24;' + '\n}'
+
         paths = {}
         for dev_x in devices:
             for dev_y in devices:
                 for cve_dev_x in io_desc[dev_x]:
                     for cve_dev_y in io_desc[dev_y]: 
                         try:
-                            paths[str(cve_dev_x["id"])+","+str(cve_dev_y["id"])] = nx.shortest_path(G, source=cve_dev_x["id"], target=cve_dev_y["id"])
+                            paths[str(cve_dev_x["id"])+","+str(cve_dev_y["id"])] = nx.shortest_path(SchematicGraph, source=cve_dev_x["id"], target=cve_dev_y["id"])
                         except:
                             paths[str(cve_dev_x["id"])+","+str(cve_dev_y["id"])] = "None"
-        print ("All paths: ",paths)
-    return paths, G, edge_labels, dotstr
+
+        # print ("All paths: ",paths)
+    return paths, SchematicGraph, ImpactGraph, ExploitabilityGraph, edge_labels, schematic_dotstr, impact_dotstr, exploitability_dotstr
 
 '''
-A network is an undirected graph with maximum diameter = 2 where vertices are IoT devices in a 
+A network is an undirected graph with diameter d <= 2 where vertices are IoT devices in a 
 home and edges represent communication. There is a central vertex in each graph that is adjacent 
 to each other edge that represents the router.
 ''' 
 
 def buildNetwork(devices):
-    vectors = {}
+    vector = {}
     home_network = snap.TUNGraph.New()
     labels = snap.TIntStrH()
     dev_ind = 0
     for device in devices:
         dev_ind+=1
-        vector = buildVector(device)
-        vectors[device] = vector
         home_network.AddNode(dev_ind)
         if dev_ind!=1:
-            home_network.AddEdge(dev_ind, 1) # assuming Router is the first device in the argument
+            home_network.AddEdge(dev_ind, 1)
         labels.AddDat(dev_ind, str(device))
-    return home_network, labels, vectors
+        vector = buildVector(device,vector)
+    return home_network, labels, vector
 
 if __name__ == "__main__":
     parser = OptionParser()
@@ -157,16 +232,37 @@ if __name__ == "__main__":
 
      ### build ###
 
-    home_network, network_labels, vectors = buildNetwork(devices)
-    paths, G, edge_labels, dotstr = buildCircuit(devices)
+    home_network, network_labels, vector = buildNetwork(devices)
+    # print(vector)
+    paths, SchematicGraph, ImpactGraph, ExploitabilityGraph, edge_labels, schematic_dotstr, impact_dotstr, exploitability_dotstr = buildCircuit(devices,vector)
 
-    ### draw/save ###
+    ### find max flow values ###
+
+    max_impact = nx.maximum_flow_value(ImpactGraph, "Non-CVE info: Router", "Attacker")
+    max_exploitability = nx.maximum_flow_value(ExploitabilityGraph, "Non-CVE info: Router", "Attacker")
+    print("Max Impact: ",max_impact,"Max Exploitability: ",max_exploitability)
+
+    ### save ###
+
     snap.DrawGViz(home_network, snap.gvlDot, "home_network.png", "Home Network", network_labels)
-    dotfile = open("circuit.dot", "w")
-    dotfile.write(dotstr)
-    dotfile.close()
-    (graph,) = pydot.graph_from_dot_file('circuit.dot')
-    graph.write_png('circuit.png')
+    schematic_dotfile = open("schematic_circuit.dot", "w")
+    schematic_dotfile.write(schematic_dotstr)
+    schematic_dotfile.close()
+    impact_dotfile = open("impact_circuit.dot", "w")
+    impact_dotfile.write(impact_dotstr)
+    impact_dotfile.close()
+    exploitability_dotfile = open("exploitability_circuit.dot", "w")
+    exploitability_dotfile.write(exploitability_dotstr)
+    exploitability_dotfile.close()
+
+    ### draw ###
+
+    (schematic_graph,) = pydot.graph_from_dot_file('schematic_circuit.dot')
+    schematic_graph.write_png('schematic_circuit.png')
+    (impact_graph,) = pydot.graph_from_dot_file('impact_circuit.dot')
+    impact_graph.write_png('impact_circuit.png')
+    (exploitability_graph,) = pydot.graph_from_dot_file('exploitability_circuit.dot')
+    exploitability_graph.write_png('exploitability_circuit.png')
 
     # nx_node_labels = nx.draw_networkx_labels(G,pos=graphviz_layout(G, prog='dot'))
     # nx_edge_labels = nx.draw_networkx_edge_labels(G,pos=graphviz_layout(G, prog='dot'),edge_labels=edge_labels,font_color='red') 
--- collectCVEs.py
+++ collectCVEs.py
@@ -13,8 +13,6 @@ with open('devicemap.json') as device_json:
                         dev_map[device["device"]].append(cve)
                         print(cve)
     print dev_map
-# now write output to a file
 descfile = open("known_cves.json", "w")
-# magic happens here to make it pretty-printed
 descfile.write(json.dumps(dev_map, indent=4, sort_keys=True))
 descfile.close()

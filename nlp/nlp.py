e5e02b827f1e39b3643846a6cc960179684a6dfb
Karan K. Budhraja <karanb1@umbc.edu>
2019-01-05 13:45:03 -0500

    wip: use tfidf to determine importance of terms

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -7,6 +7,9 @@ from nltk.tokenize.treebank import TreebankWordDetokenizer
 from nltk.stem import PorterStemmer
 import re
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
 # reduce text to comparable format
 def clean_text(text):
     # text preprocessing
@@ -43,12 +46,14 @@ def load_data():
     with open('../descriptions_io.json', 'r') as inFile:
         data = json.loads(inFile.read())
 
+    allCleanedDescriptions = []
     for deviceName in data.keys():
         for deviceData in data[deviceName]:
             description = deviceData['description']
             cleanedDescription = clean_text(description)
             deviceData['cleaned_description'] = cleanedDescription
-
+            allCleanedDescriptions.append(cleanedDescription)
+            
             iOList = deviceData['i/o']
             if(len(iOList) == 0):
                 # no processing for empty list
@@ -64,14 +69,49 @@ def load_data():
                 cleanedIOList.append(cleanedPair)
             deviceData['cleaned_i/o'] = cleanedIOList
 
-
-
-
-
+    # return modified data
+    # cleaned components have been added
+    return [data, allCleanedDescriptions] 
             
-description = "An issue was discovered in D-Link 'myDlink Baby App' version 2.04.06. Whenever actions are performed from the app (e.g., change camera settings or play lullabies), it communicates directly with the Wi-Fi camera (D-Link 825L firmware 1.08) with the credentials (username and password) in base64 cleartext. An attacker could conduct an MitM attack on the local network and very easily obtain these credentials."
+[data, allCleanedDescriptions] = load_data()
 
-#cleanedDescription = clean_text(description)
-#print(cleanedDescription)
+# compute tf idf scores
+tfIdfVectorizer = TfidfVectorizer()
+tfIdfMatrix = tfIdfVectorizer.fit_transform(allCleanedDescriptions)
+features = tfIdfVectorizer.get_feature_names()
+featuresIdf = tfIdfVectorizer.idf_
 
-load_data()
+# get all features in reverse idf order
+# idf can be a heuristic to extract important terms
+sortedIndexes = np.argsort(featuresIdf)[::-1]
+
+for index in sortedIndexes:
+    feature = features[index]
+    idf = featuresIdf[index]
+
+    # use top k terms here
+    #print(feature, idf)
+
+# process cleaned descriptions
+for cleanedDescriptionIndex in range(len(allCleanedDescriptions)):
+    cleanedDescription = allCleanedDescriptions[cleanedDescriptionIndex]
+    tokens = cleanedDescription.split(' ')
+
+    tfIdfValues = []
+    for token in tokens:
+
+        # mandate minimum token length of 2 to remove invalid tokens
+        if(len(token) == 1):
+            continue
+
+        # all tokens that reach this line can be found in features
+        tokenIndex = features.index(token)
+        tfIdfValue = tfIdfMatrix[cleanedDescriptionIndex,tokenIndex]
+        tfIdfValues.append(tfIdfValue)
+
+    # use tf idf to extract important terms
+    sortedIndexes = np.argsort(tfIdfValues)[::-1]
+    sortedTokens = [tokens[index] for index in sortedIndexes]
+
+    # use top k terms here
+    print(sortedTokens)

605cabb1b400d15d01b9cb60146f1e8e12a79af5
Karan K. Budhraja <karanb1@umbc.edu>
2019-01-19 14:03:44 -0500

    add cleanup code

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -14,6 +14,7 @@ import numpy as np
 import json
 
 import sys
+import os
 
 # reduce text to comparable format
 def clean_text(text):
@@ -163,3 +164,6 @@ for item in data:
             for rl in pytextrank.normalize_key_phrases('stage1_output.json', ranks):
                 outFile.write("%s\n" % pytextrank.pretty_print(rl._asdict()))
                 print(pytextrank.pretty_print(rl))
+
+        # cleanup
+        os.system('rm -f sub_item.json stage1_output.json stage2_output.json graph.dot')
